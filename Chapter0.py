843ea9560cb9a6166b28466eb3acf20e11bb0989
unknown <anshikachaudhary769@gmail.com>
2024-04-27 11:44:42 +0530

    introduction

---
+++ Chapter0.py
@@ -0,0 +1,19 @@
+def Chapter0():
+    intro = ("""These are my comprehensive notes of python using coding. I will cover all the theoretical and practicle part here through code.
+So lets begin this fun journey with lots of positive hopes and zeal.""")
+    chap = "Chapter 0 : Introduction of programming language"
+    what = """What is programming language?
+Programming language is the set of instruction given to machines(computer) by user to perform some tasks.
+Basically we have code as programming language  (high lang) which is converted into machine language(low lang 0and1) 
+by interpreter/compiler that works as the translator between the two."""
+    abt = """Python is a programming language that is developed by 'GUIDO VAN ROSSUM' in 1991.
+It is very popular due to its following features"""
+    features="""1): INTERPRETER: directly run the program by converting source code to intermediate code internally
+    2): PLATFORM INDEPENDENT : can be run on any operating system.
+    3): FREE AND OPEN SOURCE : redistributable.
+    4): HIGH LEVEL LANGUAGE : simple, easy to learn, shorter.
+    5): ROBUST : Exceptional handling and memory management in built.
+    6): RICH LIBRARY SUPPORT :
+    7): DYNAMICALLY TYPED: """
+    print(intro,"\n", chap, "\n" ,what,"\n", abt, "\n" ,features)
+Chapter0()
