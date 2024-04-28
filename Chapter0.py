7272e598dbcb301a5dde821a591396eed1bc61c3
unknown <anshikachaudhary769@gmail.com>
2024-04-28 23:00:48 +0530

    introduction

--- Chapter0.py
+++ Chapter0.py
@@ -1,4 +1,4 @@
-def Chapter0():
+def IntroOfPython():
     intro = ("""These are my comprehensive notes of python using coding. I will cover all the theoretical and practicle part here through code.
 So lets begin this fun journey with lots of positive hopes and zeal.""")
     chap = "Chapter 0 : Introduction of programming language"
@@ -15,5 +15,9 @@ It is very popular due to its following features"""
     5): ROBUST : Exceptional handling and memory management in built.
     6): RICH LIBRARY SUPPORT :
     7): DYNAMICALLY TYPED: """
-    print(intro,"\n", chap, "\n" ,what,"\n", abt, "\n" ,features)
-Chapter0()
+    installation = """http://python.org/. it is the official website through which we can install latest version of python.
+     We can use any interpreter for better experience as per your convenience among VsCode or pycharm"""
+    first_prog ="use print function as('print anything into qoutes it will print everything as it is as string')"
+    print(intro,"\n", chap, "\n" ,what,"\n", abt, "\n" ,installation,"\n" ,features,"\n",first_prog)
+    print("For more details refer to : https://www.geeksforgeeks.org/python")
+IntroOfPython()
